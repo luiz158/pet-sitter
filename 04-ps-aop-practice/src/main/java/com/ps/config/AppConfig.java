7dcdeb1b4f1083f79b20e05b044f6d076733226e
astel <astel.develop@gmail.com>
2018-09-07 15:19:57 +0300

    aop done

--- 04-ps-aop-practice/src/main/java/com/ps/config/AppConfig.java
+++ 04-ps-aop-practice/src/main/java/com/ps/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.ps.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Created by iuliana.cosmina on 4/17/16.
@@ -9,5 +10,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {"com.ps.repos.impl", "com.ps.services" ,"com.ps.aspects"})
 //TODO 20. Enable automatic @Aspect detection
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppConfig {
 }
