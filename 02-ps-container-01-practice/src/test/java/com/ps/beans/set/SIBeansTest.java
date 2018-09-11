c82ea8e589a268b6077b2d0a2877c98db4de62e2
German Abramov <german_abramov@epam.com>
2018-09-11 17:51:54 +0300

    02 - setter injection test done

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
@@ -6,9 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Created by iuliana.cosmina on 3/26/16.
  */
@@ -25,6 +22,16 @@ public class SIBeansTest {
             logger.info("Bean " + beanName + " of type " + ctx.getBean(beanName).getClass().getSimpleName());
         }
 
-        //TODO 4. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+        ComplexBeanImpl complexBean0 = (ComplexBeanImpl) ctx.getBean("complexBean0");
+        assert (complexBean0.getSimpleBean().equals(ctx.getBean("simpleBean0")));
+
+        ComplexBeanImpl complexBean1 = (ComplexBeanImpl) ctx.getBean("complexBean1");
+        assert (complexBean1.getSimpleBean().equals(ctx.getBean("simpleBean0")));
+        assert (complexBean1.isComplex());
+
+        ComplexBean2Impl complexBean2 = (ComplexBean2Impl) ctx.getBean("complexBean2");
+        assert (complexBean2.getSimpleBean().equals(ctx.getBean("simpleBean0")));
+        assert (complexBean2.isComplex());
+
     }
 }
