dd87534b677f97b6313410798f5ba9a5c2b7fdfa
German Abramov <german_abramov@epam.com>
2018-09-12 14:35:13 +0300

    02 - add annotations

--- 02-ps-container-01-practice/src/main/java/com/ps/sample/ComplexBean2.java
+++ 02-ps-container-01-practice/src/main/java/com/ps/sample/ComplexBean2.java
@@ -2,6 +2,10 @@ package com.ps.sample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * Created by iuliana.cosmina on 3/28/16.
@@ -20,8 +24,7 @@ public class ComplexBean2 {
         this.simpleBean1 = simpleBean1;
     }
 
-    // TODO 10. Add JSR 250 attributes to control initialization and destruction of this bean
-
+    @Required
     public void setSimpleBean2(SimpleBean simpleBean2) {
         logger.info(" --> Stage 2: Calling the setter.");
         this.simpleBean2 = simpleBean2;
@@ -31,6 +34,7 @@ public class ComplexBean2 {
      * The initialization method.
      * Just for fun: it instantiates the simpleBean2 only if the current time is even.
      */
+    @PostConstruct
     private void initMethod() {
         logger.info(" --> Stage 3: Calling the initMethod.");
         long ct = System.currentTimeMillis();
@@ -42,6 +46,7 @@ public class ComplexBean2 {
     /**
      * Destroy method
      */
+    @PreDestroy
     private boolean destroyMethod() {
         logger.info(" --> Calling the destroyMethod.");
         simpleBean1 = null;
