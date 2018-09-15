5ea87a2bf51b787c1342a1c5af47c6b927af6f3d
German Abramov <german_abramov@epam.com>
2018-09-15 12:31:55 +0300

    04 - enable aspect detection

--- 04-ps-aop-practice/src/main/java/com/ps/config/AppConfig.java
+++ 04-ps-aop-practice/src/main/java/com/ps/config/AppConfig.java
@@ -2,12 +2,13 @@ package com.ps.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Created by iuliana.cosmina on 4/17/16.
  */
 @Configuration
 @ComponentScan(basePackages = {"com.ps.repos.impl", "com.ps.services" ,"com.ps.aspects"})
-//TODO 20. Enable automatic @Aspect detection
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AppConfig {
 }
