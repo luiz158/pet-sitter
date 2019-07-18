1b7b83d93c250c21446884e6491e4e2b147ee872
Dmytro Prytula <prituladima@gmail.com>
2019-07-18 22:11:59 +0300

    47

--- 10-ps-mvc-practice/src/test/java/com/ps/web/UserControllerTest.java
+++ 10-ps-mvc-practice/src/test/java/com/ps/web/UserControllerTest.java
@@ -38,6 +38,11 @@ public class UserControllerTest {
 
     @Test
     public void testFindOneHandler() throws NotFoundException {
-        // TODO 47: Complete this test for the show() method of UserController
+        ExtendedModelMap model = new ExtendedModelMap();
+        String viewName = userController.show(1L, model);
+        User user = (User) model.get("user");
+        assertNotNull(user);
+        assertEquals(Long.valueOf(1), user.getId());
+        assertEquals("users/show", viewName);
     }
 }
